2077a252342451e6f00d2880824b8461117fdb21
safi-abedin <kadersafi@2050>
2023-06-21 20:07:44 +0600

    ef migration is not working ,beacuse of autofac but ,i can't find out why autofac is genereting problems in dependency injection

--- FirstDemo.Web/Program.cs
+++ FirstDemo.Web/Program.cs
@@ -8,34 +8,37 @@ using Microsoft.EntityFrameworkCore;
 using Serilog;
 using Serilog.Events;
 using static FirstDemo.Web.WebModule;
+using System.Reflection;
+try
+{
+    var builder = WebApplication.CreateBuilder(args);
 
-var builder = WebApplication.CreateBuilder(args);
+    // Add services to the container.
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+    var assemblyName = Assembly.GetExecutingAssembly().FullName;
+    builder.Services.AddDbContext<ApplicationDbContext>(options =>
+        options.UseSqlServer(connectionString, m => m.MigrationsAssembly(assemblyName)));
+    builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-// Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+    builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+        .AddEntityFrameworkStores<ApplicationDbContext>();
+    builder.Services.AddControllersWithViews();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
-builder.Services.AddControllersWithViews();
+    //serilog
+    builder.Host.UseSerilog((ctx, lc) => lc
+                .MinimumLevel.Debug()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                .Enrich.FromLogContext()
+                .ReadFrom.Configuration(builder.Configuration));
 
-//serilog
-builder.Host.UseSerilog((ctx, lc) => lc
-            .MinimumLevel.Debug()
-            .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
-            .Enrich.FromLogContext()
-            .ReadFrom.Configuration(builder.Configuration));
-try {
 
     // Service collection use kore Dependency Injection
 
-   builder.Services.AddTransient<ICourseModel, CourseModel>();
+    builder.Services.AddTransient<ICourseModel, CourseModel>();
     //builder.Services.AddSingleton<ICourseModel, CourseModel>();
     //builder.Services.AddScoped<ICourseModel, CourseModel>();
 
-   
+
 
 
     var app = builder.Build();
@@ -47,41 +50,42 @@ try {
     builder.Host.ConfigureContainer<ContainerBuilder>(containerBuilder =>
     {
         containerBuilder.RegisterModule(new WebModule());
+        containerBuilder.RegisterModule(new InfrastructureModule(connectionString, assemblyName));
     });
 
 
     // Configure the HTTP request pipeline.
     if (app.Environment.IsDevelopment())
-{
-    app.UseMigrationsEndPoint();
-}
-else
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
+    {
+        app.UseMigrationsEndPoint();
+    }
+    else
+    {
+        app.UseExceptionHandler("/Home/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
 
-app.UseHttpsRedirection();
-app.UseStaticFiles();
+    app.UseHttpsRedirection();
+    app.UseStaticFiles();
 
-app.UseRouting();
+    app.UseRouting();
 
-app.UseAuthentication();
-app.UseAuthorization();
-
-
-app.MapControllerRoute(
-     name: "areas",
-     pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+    app.UseAuthentication();
+    app.UseAuthorization();
 
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-app.MapRazorPages();
+    app.MapControllerRoute(
+         name: "areas",
+         pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
 
-app.Run();
+
+    app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+    app.MapRazorPages();
+
+    app.Run();
 }
 catch (Exception ex)
 {
